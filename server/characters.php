1015ce4d47e609093d15455f9f2c69719d7a30ef
lave0172 <32076537+lave0172@users.noreply.github.com>
2017-12-11 08:28:55 -0500

    Midterm Project

---
+++ server/characters.php
@@ -0,0 +1,172 @@
+<?php
+  $characters = [
+    [
+      "name" => "Boo",
+      "series" => "Super Mario Bros.",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Dr. Luigi",
+      "series" => "Super Mario Bros.",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Dry Bowser",
+      "series" => "Super Mario Bros.",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Ice Climbers",
+      "series" => "Ice Climbers",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Knuckles",
+      "series" => "Sonic",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Krystal",
+      "series" => "Starfox Adventures",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Pyra",
+      "series" => "Xenoblade Chronicles",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Shadow",
+      "series" => "Sonic",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Tails",
+      "series" => "Sonic",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Toad",
+      "series" => "Super Mario Bros.",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Wolf",
+      "series" => "Starfox",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Gengar",
+      "series" => "Pokemon",
+      "image" => "",
+      "filter" => "new"
+    ],
+
+    [
+      "name" => "Mario",
+      "series" => "Super Mario Bros.",
+      "image" => "images/mario.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Donkey Kong",
+      "series" => "Donkey Kong",
+      "image" => "images/donkeykong.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Link",
+      "series" => "The Legend of Zelda",
+      "image" => "images/link.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Samus",
+      "series" => "Metroid",
+      "image" => "images/samus.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Yoshi",
+      "series" => "Super Mario Bros.",
+      "image" => "images/yoshi.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Kirby",
+      "series" => "Kirby",
+      "image" => "images/kirby.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Fox",
+      "series" => "Starfox",
+      "image" => "images/fox.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Pikachu",
+      "series" => "Pokemon",
+      "image" => "images/pikachu.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Luigi",
+      "series" => "Super Mario Bros.",
+      "image" => "images/luigi.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Captain Falcon",
+      "series" => "F-Zero",
+      "image" => "images/captainfalcon.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Ness",
+      "series" => "Earthbound",
+      "image" => "images/ness.png",
+      "filter" => "old"
+    ],
+
+    [
+      "name" => "Jigglypuff",
+      "series" => "Pokemon",
+      "image" => "images/jigglypuff.png",
+      "filter" => "old"
+    ],
+
+  ];
+ ?>
